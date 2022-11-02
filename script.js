1eb8d0f8e148e05f25ac468c6280d060e8c1a047
Andrew <115462877+parkypark92@users.noreply.github.com>
2022-11-02 12:22:23 +0000

    get user input and create function to determine winner and output result

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
 //randomly generate 'rock' 'paper' or 'scissors' as the computers choice
+
 function computerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     let currentChoice = choices[Math.floor(Math.random() * choices.length)];
     return currentChoice;
 }
-console.log(computerChoice());
-//allow user to input 'rock' 'paper' or 'scissors' (case insensitive)
 
 //play one round using computer choice and user choice, display winner
+
+    //allow user to input 'rock' 'paper' or 'scissors' (case insensitive)
+
+let userChoice = prompt("Input 'Rock', 'Paper' or 'Scissors'");
+
+    //determine who is the winner
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.replace(playerSelection[0], playerSelection[0].toUpperCase());
+
+    if(playerSelection === "Rock" && computerSelection === "Paper" ||
+    playerSelection === "Paper" && computerSelection === "Scissors" ||
+    playerSelection === "Scissors" && computerSelection === "Rock") {
+        console.log(`${computerSelection} beats ${playerSelection}, Computer wins!`);
+
+    } else if(playerSelection === "Rock" && computerSelection === "Scissors" ||
+    playerSelection === "Paper" && computerSelection === "Rock" ||
+    playerSelection === "Scissors" && computerSelection === "Paper") {
+        console.log(`${playerSelection} beats ${computerSelection}, Player wins!`);
+
+    } else if(playerSelection === "Rock" && computerSelection === "Rock" ||
+    playerSelection === "Paper" && computerSelection === "Paper" ||
+    playerSelection === "Scissors" && computerSelection === "Scissors") {
+        console.log(`${playerSelection} and ${computerSelection}, it's a tie!`)
+
+    } else {
+        console.log("Please enter Rock, Paper or Scissors!");
+    }
+    
+}
+
+    playRound(userChoice, computerChoice());
+    
+
+
+    
+    //display winner of round
 
 //loop through 5 rounds, displaying winner each time and keeping score
 
