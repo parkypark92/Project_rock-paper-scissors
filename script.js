8ddfec801e9208b8b6fe093e5e82acb9956c4fa3
Andrew <115462877+parkypark92@users.noreply.github.com>
2022-11-02 15:15:34 +0000

    add loops to play rounds until someone gets to 5 wins. Display winner

--- script.js
+++ script.js
@@ -1,52 +1,76 @@
 //randomly generate 'rock' 'paper' or 'scissors' as the computers choice
 
-function computerChoice() {
+function getComputerChoice () {
     const choices = ["Rock", "Paper", "Scissors"];
     let currentChoice = choices[Math.floor(Math.random() * choices.length)];
     return currentChoice;
 }
-
 //play one round using computer choice and user choice, display winner
-
-    //allow user to input 'rock' 'paper' or 'scissors' (case insensitive)
-
-let userChoice = prompt("Input 'Rock', 'Paper' or 'Scissors'");
-
     //determine who is the winner
-
+    //display winner of round
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    playerSelection = playerSelection.replace(playerSelection[0], playerSelection[0].toUpperCase());
+    let result = ""
 
     if(playerSelection === "Rock" && computerSelection === "Paper" ||
     playerSelection === "Paper" && computerSelection === "Scissors" ||
     playerSelection === "Scissors" && computerSelection === "Rock") {
-        console.log(`${computerSelection} beats ${playerSelection}, Computer wins!`);
+        result = "computerWin";
+        
 
     } else if(playerSelection === "Rock" && computerSelection === "Scissors" ||
     playerSelection === "Paper" && computerSelection === "Rock" ||
     playerSelection === "Scissors" && computerSelection === "Paper") {
-        console.log(`${playerSelection} beats ${computerSelection}, Player wins!`);
+        
+        result = "playerWin";
+        
 
     } else if(playerSelection === "Rock" && computerSelection === "Rock" ||
     playerSelection === "Paper" && computerSelection === "Paper" ||
     playerSelection === "Scissors" && computerSelection === "Scissors") {
-        console.log(`${playerSelection} and ${computerSelection}, it's a tie!`)
+        result = "tie";
 
-    } else {
-        console.log("Please enter Rock, Paper or Scissors!");
-    }
-    
+    } 
+    return result;
 }
 
-    playRound(userChoice, computerChoice());
-    
-
 
     
-    //display winner of round
-
 //loop through 5 rounds, displaying winner each time and keeping score
+function playGame() {
+    let playerCount = 0;
+    let computerCount = 0;
+    for(let i = 0; i < 5; i++) {
+        while(playerCount <= i && computerCount <= i) {
+//allow user to input 'rock' 'paper' or 'scissors' (case insensitive)
+let playerChoice = prompt("Input 'Rock', 'Paper' or 'Scissors'");
+playerChoice = playerChoice.toLowerCase();
+    playerChoice = playerChoice.replace(playerChoice[0], playerChoice[0].toUpperCase());
+
+let computerChoice = getComputerChoice();
+
+if(playRound(playerChoice, computerChoice) === "computerWin") {
+console.log(`${computerChoice} beats ${playerChoice}, Computer wins!`);
+computerCount++;
+} else if(playRound(playerChoice, computerChoice) === "playerWin") {
+    console.log(`${playerChoice} beats ${computerChoice}, Player wins!`);
+    playerCount++;
+} else if(playRound(playerChoice, computerChoice) === "tie") {
+    console.log(`${playerChoice} and ${computerChoice}, it's a tie!`);
+} else {
+    console.log("Please enter Rock, Paper or Scissors!");
+}
+
+console.log(`Player: ${playerCount}    Computer: ${computerCount}`);
+
+}
+    }
+    if(playerCount === 5){
+        console.log("Player wins the game!!!");
+    } else if(computerCount === 5) {
+        console.log("Computer wins the game!!!");
+    }
+}
+playGame();
 
 //first to 5 is overall winner
 
