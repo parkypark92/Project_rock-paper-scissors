b918f4a8a6265af7741321d643e8f3b8d762d79e
Andrew <115462877+parkypark92@users.noreply.github.com>
2022-11-02 15:54:10 +0000

    tidy up code

--- script.js
+++ script.js
@@ -5,24 +5,22 @@ function getComputerChoice () {
     let currentChoice = choices[Math.floor(Math.random() * choices.length)];
     return currentChoice;
 }
-//play one round using computer choice and user choice, display winner
+//play one round using computer choice and user choice
     //determine who is the winner
-    //display winner of round
+    //return winner of round
 function playRound(playerSelection, computerSelection) {
-    let result = ""
+
+let result = ""
 
     if(playerSelection === "Rock" && computerSelection === "Paper" ||
     playerSelection === "Paper" && computerSelection === "Scissors" ||
     playerSelection === "Scissors" && computerSelection === "Rock") {
         result = "computerWin";
-        
 
     } else if(playerSelection === "Rock" && computerSelection === "Scissors" ||
     playerSelection === "Paper" && computerSelection === "Rock" ||
     playerSelection === "Scissors" && computerSelection === "Paper") {
-        
-        result = "playerWin";
-        
+        result = "playerWin";    
 
     } else if(playerSelection === "Rock" && computerSelection === "Rock" ||
     playerSelection === "Paper" && computerSelection === "Paper" ||
@@ -37,33 +35,42 @@ function playRound(playerSelection, computerSelection) {
     
 //loop through 5 rounds, displaying winner each time and keeping score
 function playGame() {
-    let playerCount = 0;
-    let computerCount = 0;
+
+let playerCount = 0;
+let computerCount = 0;
+
+    //play rounds until someone gets 5 wins
     for(let i = 0; i < 5; i++) {
+        //only allow loop to increment if one score is higher than the other
         while(playerCount <= i && computerCount <= i) {
-//allow user to input 'rock' 'paper' or 'scissors' (case insensitive)
-let playerChoice = prompt("Input 'Rock', 'Paper' or 'Scissors'");
-playerChoice = playerChoice.toLowerCase();
-    playerChoice = playerChoice.replace(playerChoice[0], playerChoice[0].toUpperCase());
 
-let computerChoice = getComputerChoice();
+        //allow user to input 'rock' 'paper' or 'scissors' (case insensitive)
+        let playerChoice = prompt("Input 'Rock', 'Paper' or 'Scissors'");
+        playerChoice = playerChoice.toLowerCase();
+            playerChoice = playerChoice.replace(playerChoice[0], playerChoice[0].toUpperCase());
 
-if(playRound(playerChoice, computerChoice) === "computerWin") {
-console.log(`${computerChoice} beats ${playerChoice}, Computer wins!`);
-computerCount++;
-} else if(playRound(playerChoice, computerChoice) === "playerWin") {
-    console.log(`${playerChoice} beats ${computerChoice}, Player wins!`);
-    playerCount++;
-} else if(playRound(playerChoice, computerChoice) === "tie") {
-    console.log(`${playerChoice} and ${computerChoice}, it's a tie!`);
-} else {
-    console.log("Please enter Rock, Paper or Scissors!");
-}
+        //generate computers choice
+        let computerChoice = getComputerChoice();
 
-console.log(`Player: ${playerCount}    Computer: ${computerCount}`);
+        //play a round, display winner, keep score
+        if(playRound(playerChoice, computerChoice) === "computerWin") {
+        console.log(`${computerChoice} beats ${playerChoice}, Computer wins!`);
+        computerCount++;
+        } else if(playRound(playerChoice, computerChoice) === "playerWin") {
+            console.log(`${playerChoice} beats ${computerChoice}, Player wins!`);
+            playerCount++;
+        } else if(playRound(playerChoice, computerChoice) === "tie") {
+            console.log(`${playerChoice} and ${computerChoice}, it's a tie!`);
+        } else {
+            console.log("Please enter Rock, Paper or Scissors!");
+        }
 
-}
-    }
+        console.log(`Player: ${playerCount}    Computer: ${computerCount}`);
+        } //exit while loop
+
+    } //exit for loop
+
+    //display winner
     if(playerCount === 5){
         console.log("Player wins the game!!!");
     } else if(computerCount === 5) {
