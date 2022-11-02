af81288df5fa3c3a54a93f5efbb1c9f4bb94fb02
Andrew <115462877+parkypark92@users.noreply.github.com>
2022-11-02 11:03:56 +0000

    create function to generate computers choice

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //randomly generate 'rock' 'paper' or 'scissors' as the computers choice
-
+function computerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    let currentChoice = choices[Math.floor(Math.random() * choices.length)];
+    return currentChoice;
+}
+console.log(computerChoice());
 //allow user to input 'rock' 'paper' or 'scissors' (case insensitive)
 
 //play one round using computer choice and user choice, display winner
